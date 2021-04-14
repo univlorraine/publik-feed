2d43e5ec55b30cc0a5a90bb5c4b239bfd5f8d3ff
Charlie <charlie.dubois@univ-lorraine.fr>
2021-04-14 12:43:40 +0200

    change longueur colonne filtre

---
+++ src/main/resources/db/migration/V1.0.3__lengthFiltre.sql
@@ -0,0 +1,6 @@
+-- --------------------------------------------------------
+--   change la longueur de la colonne filtre
+-- --------------------------------------------------------
+
+ALTER TABLE role_manuel MODIFY filtre varchar(300) NULL;
+
