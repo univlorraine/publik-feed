727bccf71d0a3fe880ca0a0eb550cc9f0685d5ff
Charlie <charlie.dubois@univ-lorraine.fr>
2024-04-02 10:26:08 +0200

    changment cron NewUsersSyncJob pour éviter les pb lors des changemetns d'heure

--- src/main/java/fr/univlorraine/publikfeed/utils/CronUtils.java
+++ src/main/java/fr/univlorraine/publikfeed/utils/CronUtils.java
@@ -44,7 +44,7 @@ public final class CronUtils {
 
 	public static final String CRON_SYNC_USERS = "0 50 7 * * ?";
 	
-	public static final String CRON_SYNC_NEW_USERS = "0 0 * * * ?";
+	public static final String CRON_SYNC_NEW_USERS = "0 0 6-22 * * ?";
 	
 	public static final String CRON_SYNC_SUPPR_USERS = "0 50 8 * * ?";
 	
